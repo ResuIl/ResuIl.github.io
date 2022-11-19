81761c18a1c3794f5cf4029a952a46d2ce9fdf50
Rəsul <71267982+ResuIl@users.noreply.github.com>
2022-11-19 14:06:13 +0400

    New Files

---
+++ js/commands.js
@@ -0,0 +1,89 @@
+var youtube = "https://www.youtube.com/channel/UCAjUoh7E16J67G-Y9leGhWg";
+var password = "resul123";
+var instagram = "https://www.instagram.com/r3s7l/";
+var github = "https://github.com/ResuIl/";
+
+whois = [
+  "<br>",
+  "Hey, I'm Resul!👋",
+  "I am 16 years old.",
+  "I am trying to improve myself in software programming. Coding is a piece of cake. 😊",
+  "I am student at StepIT Academy and My First Professional Teacher is <b>Tural</b> helping me improve my experience.",
+  "Don't forget using 2FA 😉",
+  "My Aim: Senior Developer.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/Oxygen Products' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/r3s7l' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/ResuIl' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Most projects are private on my Github.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Resul?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+
+banner = [
+    "                          oooo$$$$$$$$$$$$oooo",
+    "                      oo$$$$$$$$$$$$$$$$$$$$$$$$o",
+    "                   oo$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o         o$   $$ o$",
+    "   o $ oo        o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o       $$ $$ $$o$",
+    "oo $ $ \"$      o$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$o       $$$o$$o$",
+    "\"$$$$$$o$     o$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$o    $$$$$$$$",
+    "  $$$$$$$    $$$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$$$$$$$$$$$$$$",
+    "  $$$$$$$$$$$$$$$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$$$$$$  \"\"\"$$$",
+    "   \"$$$\"\"\"\"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     \"$$$",
+    "    $$$   o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     \"$$$o",
+    "   o$$\"   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$       $$$o",
+    "   $$$    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\" \"$$$$$$ooooo$$$$o",
+    "  o$$$oooo$$$$$  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   o$$$$$$$$$$$$$$$$$",
+    "  $$$$$$$$\"$$$$   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     $$$$\"\"\"\"\"\"\"\"",
+    " \"\"\"\"       $$$$    \"$$$$$$$$$$$$$$$$$$$$$$$$$$$$\"      o$$$",
+    "            \"$$$o     \"\"\"$$$$$$$$$$$$$$$$$$\"$$\"         $$$",
+    "              $$$o          \"$$\"\"$$$$$$\"\"\"\"           o$$$",
+    "               $$$$o                 oo             o$$$\"",
+    "                \"$$$$o      o$$$$$$o\"$$$$o        o$$$$",
+    "                  \"$$$$$oo     \"\"$$$$o$$$$$o   o$$$$\"\"  ",
+    "                     \"\"$$$$$oooo  \"$$$o$$$$$$$$$\"\"\"",
+    "                        \"\"$$$$$$$oo $$$$$$$$$$       ",
+    "                                \"\"\"\"$$$$$$$$$$$        ",
+    "                                    $$$$$$$$$$$$       ",
+    "                                     $$$$$$$$$$\"      ",
+    "                                      \"$$$\"\"\"\"",
+    "                                                             ",
+    '<span class="color2">Welcome to my interactive web terminal.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+]
